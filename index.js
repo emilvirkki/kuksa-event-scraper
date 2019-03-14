97a0b4f20797da13c047434fa525fed956b1ca4d
Emil Virkki <emil.virkki@gmail.com>
2019-03-14 22:34:41 +0200

    Parse dates and make error handling more robust

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-// TODO Add readme
-// TODO add try-catch-finally blocks so that the browser won't be left open
 // TODO Add exports
 // TODO Add test script that lists and pulls a couple of events
+// TODO Add readme
 // TODO publish on npm
 // TODO add ability to filter events by dates
 
 const puppeteer = require('puppeteer');
+const moment = require('moment');
 
 const parseDateTime = (dateTimeStr) => {
-  // TODO Actually parse the string to datetime
-  return dateTimeStr;
+  if (dateTimeStr.indexOf('klo') > -1) {
+    return moment(dateTimeStr, 'DD.MM.YYYY [klo] HH:mm').toDate();
+  } else {
+    return moment(dateTimeStr, 'DD.MM.YYYY').hour(12).minute(0).second(0).toDate();
+  }
 };
 
 const getEvents = async (filters) => {
@@ -17,15 +20,21 @@ const getEvents = async (filters) => {
     throw new Error('Organizer ID must be an integer');
   }
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  let browser, links;
+  try {
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  await page.goto('https://kuksa.partio.fi/Kotisivut/tilaisuudet.aspx');
-  await page.select('#dpJarjestajaId', filters.organizer);
-  await page.click('#btnHae');
-  await page.waitForNavigation();
-  const links = await page.$$eval('.varilinkki', links => links.map(a => a.href));
-  await browser.close();
+    await page.goto('https://kuksa.partio.fi/Kotisivut/tilaisuudet.aspx');
+    await page.select('#dpJarjestajaId', filters.organizer);
+    await page.click('#btnHae');
+    await page.waitForNavigation();
+    links = await page.$$eval('.varilinkki', links => links.map(a => a.href));
+  } catch (e) {
+    throw new Error(`Finding events failed due to puppeteer error: ${e}`);
+  } finally {
+    await browser.close();
+  }
 
   return links.map(url => {
     const id = +url.split('=')[1];
@@ -42,36 +51,36 @@ const getEventInfo = async (eventId) => {
     throw new Error('Event ID must be an integer');
   }
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-    await page.setViewport({
-    width: 1200,
-    height: 1400,
-  });
+  let browser, res;
+  try {
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://kuksa.partio.fi/Kotisivut/tilaisuus_tiedot.aspx?TIAId=${eventId}`);
 
-  await page.goto(`https://kuksa.partio.fi/Kotisivut/tilaisuus_tiedot.aspx?TIAId=${eventId}`);
+    const datesString = await page.$eval('#lblAjankohta', e => e.textContent);
+    const dates = datesString.split(' - ');
+    // Only dates are known, not exact times
+    const onlyDatesAvailable = datesString.indexOf('klo') === -1;
 
-  const datesString = await page.$eval('#lblAjankohta', e => e.textContent);
-  const dates = datesString.split(' - ');
-  // Only dates are known, not exact times
-  const onlyDatesAvailable = datesString.indexOf('klo') === -1;
-
-  const res = {
-    id: eventId,
-    name: (await page.$eval('#lblTilaisuus', e => e.textContent)) || null,
-    organizer: (await page.$eval('#lblJarjestajat', e => e.textContent)) || null,
-    dateTimeStarts: parseDateTime(dates[0]),
-    dateTimeEnds: parseDateTime(dates[1]),
-    onlyDatesAvailable: onlyDatesAvailable,
-    registrationEnds: parseDateTime(await page.$eval('#lblIlmoPaattyy', e => e.textContent)),
-    lateRegistrationEnds: parseDateTime(await page.$eval('#lblJalkiIlmoPvm', e => e.textContent)),
-    location: (await page.$eval('#lblPaikka', e => e.textContent)) || null,
-    eventType: (await page.$eval('#lblTapahtumanTyyppi', e => e.textContent)) || null,
-    ageGroup: (await page.$eval('#lblIkakausi', e => e.textContent)) || null,
-    description: (await page.$eval('#lblKuvaus', e => e.textContent)) || null,
-  };
-
-  await browser.close();
+    res = {
+      id: eventId,
+      name: (await page.$eval('#lblTilaisuus', e => e.textContent)) || null,
+      organizer: (await page.$eval('#lblJarjestajat', e => e.textContent)) || null,
+      dateTimeStarts: parseDateTime(dates[0]),
+      dateTimeEnds: parseDateTime(dates[1]),
+      onlyDatesAvailable: onlyDatesAvailable,
+      registrationEnds: parseDateTime(await page.$eval('#lblIlmoPaattyy', e => e.textContent)),
+      lateRegistrationEnds: parseDateTime(await page.$eval('#lblJalkiIlmoPvm', e => e.textContent)),
+      location: (await page.$eval('#lblPaikka', e => e.textContent)) || null,
+      eventType: (await page.$eval('#lblTapahtumanTyyppi', e => e.textContent)) || null,
+      ageGroup: (await page.$eval('#lblIkakausi', e => e.textContent)) || null,
+      description: (await page.$eval('#lblKuvaus', e => e.textContent)) || null,
+    };
+  } catch (e) {
+    throw new Error(`Finding event info failed due to puppeteer error: ${e}`);
+  } finally {
+    await browser.close();
+  }
   return res;
 };
 
